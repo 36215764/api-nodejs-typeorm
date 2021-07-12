060fc7a4ed33f8df5eec88e018d4f65e0631c966
Richard Guesso <richardguesso57@gmail.com>
2021-07-12 14:01:20 -0300

    teste

--- ormconfig.js
+++ ormconfig.js
@@ -2,6 +2,12 @@ module.exports = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
   synchronize: false,
+  ssl: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
   logging: false,
   entities: ['dist/models/**/*.js'],
   migrations: ['dist/migrations/**/*.js'],
